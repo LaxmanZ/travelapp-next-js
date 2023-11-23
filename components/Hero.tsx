3e83fe0989b72da08497e6a93530e4bc1f1f6642
LaxmanZ <bhajantrilaxman2001@gmail.com>
2023-11-23 15:39:02 +0530

    updated som styling in global css

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -57,6 +57,7 @@ const Hero = () => {
         </div>
       </div>
 
+      {/* Right  */}
       <div className="relative flex flex-1 items-start">
         <div className="relative z-20 w-[268px]  flex flex-col gap-8 rounded-3xl bg-green-90 px-7 py-8">
           <div className="flex flex-col">
