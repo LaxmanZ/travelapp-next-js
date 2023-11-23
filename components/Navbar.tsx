4ce3e5d06efaf1c170ac0f8edc6357c92a51c560
LaxmanZ <bhajantrilaxman2001@gmail.com>
2023-11-23 14:20:20 +0530

    added assets, constants & creating Navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,18 @@
-import React from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav className=" flexBetween max-container padding-container relative z-30 py-5">
+      <Link href="/">
+        <Image src="/ZoroTravels-logo.png" alt="logo" width={80} height={30} />
+      </Link>
+
+      <ul className="hidden h-full gap-12 lg:flex">
+        
+      </ul>
+    </nav>
+  );
 };
 
 export default Navbar;
