150ebd25a592b6fa9407596aae599d1333025306
LaxmanZ <bhajantrilaxman2001@gmail.com>
2023-11-24 15:51:31 +0530

    designed the Footer layout

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,9 +1,87 @@
-import React from 'react'
+import { FOOTER_CONTACT_INFO, FOOTER_LINKS, SOCIALS } from '@/constants';
+import { time } from 'console';
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react';
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer className="flexCenter mb-24">
+      <div className="padding-container max-container flex w-full flex-col gap-14">
+        <div className="flex flex-col items-start justify-center gap-[10%] md:flex-row">
+          <Link href="/" className="mb-10">
+            <Image
+              src="/ZoroTravels-logo.png"
+              alt="logo"
+              width={80}
+              height={30}
+            />
+          </Link>
 
-export default Footer
+          <div className="flex flex-wrap gap-10 sm:justify-between md:flex-1">
+            {FOOTER_LINKS.map((colos) => (
+              <FooterColos title={colos.title}>
+                <ul className="regular-14 flex flex-col gap-4 text-gray-30">
+                  {colos.links.map((link, id) => (
+                    <Link href="/" key={id}>
+                      {link}
+                    </Link>
+                  ))}
+                </ul>
+              </FooterColos>
+            ))}
+
+            <div className="flex flex-col gap-5">
+              <FooterColos title={FOOTER_CONTACT_INFO.title}>
+                {FOOTER_CONTACT_INFO.links.map((link, index) => (
+                  <Link
+                    href="/"
+                    key={index}
+                    className="flex gap-4 md:flex-col lg:flex-row"
+                  >
+                    <p className="whitespace-nowrap">{link.label}: </p>
+                    <p className="medium-14 whitespace-nowrap">{link.value}</p>
+                  </Link>
+                ))}
+              </FooterColos>
+            </div>
+
+            <div className="flex flex-col gap-5">
+              <FooterColos title={SOCIALS.title}>
+                <ul className="regular flex gap-4 text-gray-30">
+                  {SOCIALS.links.map((link, i) => (
+                    <Link href="/" key={i}>
+                      <Image src={link} alt="logo" width={24} height={24} />
+                    </Link>
+                  ))}
+                </ul>
+              </FooterColos>
+            </div>
+          </div>
+        </div>
+
+        <div className="border bg-gray-20" />
+
+        <p className="regular-14 w-full text-center text-gray-30">
+          2023 ZoroTravels | All rights reserved
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+type FooterColumProps = {
+  title: string;
+  children: React.ReactNode;
+};
+
+const FooterColos = ({ title, children }: FooterColumProps) => {
+  return (
+    <div className="flex flex-col gap-5">
+      <h4 className="bold-18 whitespace-nowrap">{title}</h4>
+      {children}
+    </div>
+  );
+};
+
+export default Footer;
--- constants/index.tsx
+++ constants/index.tsx
@@ -68,8 +68,8 @@ export const FOOTER_LINKS = [
 export const FOOTER_CONTACT_INFO = {
   title: 'Contact Us',
   links: [
-    { label: 'Admin Officer', value: '123-456-7890' },
-    { label: 'Email Officer', value: 'hilink@akinthil.com' },
+    { label: 'Admin Officer', value: '+91 9606163548' },
+    { label: 'Email Officer', value: 'zorolucky@gmail.com' },
   ],
 };
 
